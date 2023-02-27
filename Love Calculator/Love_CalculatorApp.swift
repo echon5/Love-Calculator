7c46212c99fde938de11083f4862e607c8de9e05
Erin Chon <echon@bsd220.org>
2023-02-27 09:34:59 -0600

    Initial Commit

---
+++ Love Calculator/Love_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_CalculatorApp.swift
+//  Love Calculator
+//
+//  Created by Erin Chon on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Love_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
